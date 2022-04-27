c298f62b55764be2870989de7ec49b5c685b8702
Ofek-Lutzky <ofeklutzky@gmail.com>
2022-04-27 20:06:53 +0300

    finish part A need to check the way they want things and functions names

--- MazeProject/src/algorithms/mazeGenerators/Maze.java
+++ MazeProject/src/algorithms/mazeGenerators/Maze.java
@@ -14,9 +14,9 @@ public class Maze {
     private int[][] board;
 //    0 = not visited
 //    1 = Wall
-//    2 = Visited
-//    8 = Start node
-//    9 = target node
+//    S = start
+//    E = End
+
 
     public Maze(Position startPosition, Position goalPosition, int[][] board) {
         this.startPosition = startPosition;
@@ -33,32 +33,54 @@ public class Maze {
     }
 
     public void print(){
-        String[][] toPrint = new String[board.length][board[0].length];
-        for (int i = 0; i < this.board.length; i++){
-            for (int j = 0; j < this.board[0].length; j++){
-                if (i == this.getStartPosition().getRowIndex() && j == this.getStartPosition().getColumnIndex())
-                    toPrint[i][j] = ANSI_GREEN +"S" + ANSI_RESET;
-                else if(i == this.getGoalPosition().getRowIndex() && j == this.getGoalPosition().getColumnIndex())
-                    toPrint[i][j] = ANSI_RED + "E" + ANSI_RESET;
-                else{
-                    if (this.board[i][j] == 1){
 
-                        toPrint[i][j] = "▓";
-                    }
-                    else {
-                        toPrint[i][j] = " ";
-                    }
+        //A nice presentation for us
 
-//                    toPrint[i][j] = String.valueOf(this.board[i][j]);
-                }
-
-            }
-        }
-
-        for (int i = 0; i < this.board.length; i++){
-            System.out.println(Arrays.toString(toPrint[i]));
-        }
+//        String[][] toPrint = new String[board.length][board[0].length];
+//        for (int i = 0; i < this.board.length; i++){
+//            for (int j = 0; j < this.board[0].length; j++){
+//                if (i == this.getStartPosition().getRowIndex() && j == this.getStartPosition().getColumnIndex())
+//                    toPrint[i][j] = ANSI_GREEN +"S" + ANSI_RESET;
+//                else if(i == this.getGoalPosition().getRowIndex() && j == this.getGoalPosition().getColumnIndex())
+//                    toPrint[i][j] = ANSI_RED + "E" + ANSI_RESET;
+//                else{
+//                    if (this.board[i][j] == 1){
+//
+//                        toPrint[i][j] = "▓";
+//                    }
+//                    else {
+//                        toPrint[i][j] = " ";
+//                    }
+//
+////                    toPrint[i][j] = String.valueOf(this.board[i][j]);
+//                }
+//
+//            }
+//        }
+////
+//        for (int i = 0; i < this.board.length; i++){
+//            System.out.println(Arrays.toString(toPrint[i]));
+//        }
 
         //todo S as the start and E as the exit check about if the print of them is here or in position
+
+        //todo
+
+        System.out.print("{");
+        for (int i = 0; i < this.board.length; i++){
+            System.out.print("{");
+            for (int j = 0; j < this.board[0].length; j++){
+                if (j<this.board.length-1){
+                    System.out.print(String.valueOf(this.board[i][j])+",");
+                }
+                else{
+                    System.out.print(String.valueOf(this.board[i][j]));
+                }
+
+
+            }
+            System.out.println("},");
+        }
+        System.out.print("};\n");
     }
 }
