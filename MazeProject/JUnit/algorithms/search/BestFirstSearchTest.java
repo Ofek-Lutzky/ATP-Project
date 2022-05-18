4775fe29d252daae559a127831e629237470bf85
noam1368 <noam1368@gmail.com>
2022-05-18 15:55:13 +0300

    create the JUnit

--- MazeProject/JUnit/algorithms/search/BestFirstSearchTest.java
+++ MazeProject/JUnit/algorithms/search/BestFirstSearchTest.java
@@ -26,16 +26,16 @@ class BestFirstSearchTest {
     void solve() {
         int[][] board = {{0,1,0},{0,1,0},{0,0,0}};
         Maze maze = new Maze(new Position(0,0), new Position(0,2), board);
+        //maze.print();
         Solution sol = bfs.solve(new SearchableMaze(maze));
+        //sol.print(maze);
         ArrayList<AState> checker = new ArrayList<>();
         checker.add(new MazeState(new Position(0,2)));
         checker.add(new MazeState(new Position(1,2)));
-        checker.add(new MazeState(new Position(2,2)));
         checker.add(new MazeState(new Position(2,1)));
-        checker.add(new MazeState(new Position(2,0)));
         checker.add(new MazeState(new Position(1,0)));
 
-        for(int i=0; i<6; i++){
+        for(int i=0; i<4; i++){
             assertEquals(checker.get(i),sol.getSolutionPath().get(i));
         }
     }
