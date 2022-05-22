9a3dd030fc3999d602f7b401e7d5b43cb757e92d
Ofek-Lutzky <ofeklutzky@gmail.com>
2022-05-22 16:15:30 +0300

    java doc

--- MazeProject/src/algorithms/mazeGenerators/IMazeGenerator.java
+++
@@ -1,9 +0,0 @@
-package algorithms.mazeGenerators;
-
-/**
- * to implement in other classes
- */
-public interface IMazeGenerator {
-    public Maze generate(int rows, int columns);
-    public long measureAlgorithmTimeMillis(int rows, int columns);
-}
--- MazeProject/src/algorithms/search/ISearchingAlgorithm.java
+++
@@ -1,8 +0,0 @@
-package algorithms.search;
-
-public interface ISearchingAlgorithm {
-    public Solution solve(ISearchable s);
-    public int getNumberOfNodesEvaluated();
-    public String getName();
-
-}
