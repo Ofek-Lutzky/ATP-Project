67928049b0123a9684b1dcba0ed834a4f1cf0521
noam1368 <noam1368@gmail.com>
2022-05-22 14:17:13 +0300

    add tests

--- MazeProject/src/test/RunSearchOnMaze.java
+++ MazeProject/src/test/RunSearchOnMaze.java
@@ -9,23 +9,21 @@ import java.util.ArrayList;
 public class RunSearchOnMaze {
     public static void main(String[] args) {
         IMazeGenerator mg = new MyMazeGenerator();
-        Maze maze = mg.generate(40, 40);
-//        maze.print();
+        Maze maze = mg.generate(30, 30);
         SearchableMaze searchableMaze = new SearchableMaze(maze);
-//        solveProblem(searchableMaze, new BreadthFirstSearch(),maze);
-        solveProblem(searchableMaze, new BestFirstSearch(),maze);
-        solveProblem(searchableMaze, new BreadthFirstSearch(),maze);
-        solveProblem(searchableMaze, new DepthFirstSearch(), maze);
-
+        solveProblem(searchableMaze, new BreadthFirstSearch());
+        solveProblem(searchableMaze, new DepthFirstSearch());
+        solveProblem(searchableMaze, new BestFirstSearch());
     }
-    private static void solveProblem(ISearchable domain, ISearchingAlgorithm searcher, Maze maze) {//todo don't forget to remove the maze from signeture
-        //Solve a searching problem with a searcher
+    private static void solveProblem(ISearchable domain, ISearchingAlgorithm
+            searcher) {
+//Solve a searching problem with a searcher
         Solution solution = searcher.solve(domain);
-        System.out.println(String.format("'%s' algorithm - nodes evaluated: %s", searcher.getName(), searcher.getNumberOfNodesEvaluated()));
-        //Printing Solution Path
-        System.out.println("Solution path:");
+        System.out.println(String.format("'%s' algorithm - nodes evaluated: %s",
+                searcher.getName(), searcher.getNumberOfNodesEvaluated()));
+//Printing Solution Path
+                System.out.println("Solution path:");
         ArrayList<AState> solutionPath = solution.getSolutionPath();
-        solution.print(maze);
         for (int i = 0; i < solutionPath.size(); i++) {
             System.out.println(String.format("%s. %s",i,solutionPath.get(i)));
         }
