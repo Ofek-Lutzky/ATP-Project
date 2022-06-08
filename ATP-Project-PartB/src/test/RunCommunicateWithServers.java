bb1eaf2b1dc4206ac8295eedbdb83883ea12f72b
Ofek-Lutzky <ofeklutzky@gmail.com>
2022-06-08 13:37:35 +0300

    Last

--- ATP-Project-PartB/src/test/RunCommunicateWithServers.java
+++ ATP-Project-PartB/src/test/RunCommunicateWithServers.java
@@ -1,7 +1,6 @@
 package test;
 
-import Client.Client;
-import Client.IClientStrategy;
+import Client.*;
 import IO.MyDecompressorInputStream;
 import Server.Server;
 import Server.ServerStrategyGenerateMaze;
@@ -41,8 +40,7 @@ public class RunCommunicateWithServers {
 
     private static void CommunicateWithServer_MazeGenerating() {
         try {
-            Client client = new Client(InetAddress.getLocalHost(), 5400, new
-                    IClientStrategy() {
+            Client client = new Client(InetAddress.getLocalHost(), 5400, new IClientStrategy() {
                         @Override
                         public void clientStrategy(InputStream inFromServer, OutputStream outToServer) {
                             try {
@@ -54,7 +52,7 @@ public class RunCommunicateWithServers {
                                 toServer.flush();
                                 byte[] compressedMaze = (byte[]) fromServer.readObject(); //read generated maze (compressed with MyCompressor) from server
                                 InputStream is = new MyDecompressorInputStream(new ByteArrayInputStream(compressedMaze));
-                                byte[] decompressedMaze = new byte[2500 /*CHANGE SIZE ACCORDING TO YOU MAZE SIZE*/]; //allocating byte[] for the decompressed maze -
+                                byte[] decompressedMaze = new byte[1000 /*CHANGE SIZE ACCORDING TO YOU MAZE SIZE*/]; //allocating byte[] for the decompressed maze -
                                 is.read(decompressedMaze); //Fill decompressedMaze with bytes
                                 Maze maze = new Maze(decompressedMaze); maze.print();
                             } catch (Exception e) {
