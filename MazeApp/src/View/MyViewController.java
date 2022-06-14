95ea9ea49a8991984bfdd78a6df4770f1e53e0ec
Ofek-Lutzky <ofeklutzky@gmail.com>
2022-06-14 09:09:10 +0300

    javafx

--- MazeApp/src/View/MyViewController.java
+++ MazeApp/src/View/MyViewController.java
@@ -1,6 +1,25 @@
 package View;
 
 public class MyViewController implements IView{
+    @Override
+    public void mazeGenerated() {
+
+    }
+
+    @Override
+    public void mazeSolved() {
+
+    }
+
+    @Override
+    public void mazeLoaded() {
+
+    }
+
+    @Override
+    public void playerMoved() {
+
+    }
     /**
      * the functions that the button are doing
      */
